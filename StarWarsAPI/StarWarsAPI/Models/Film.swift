e150ba9334bfe3534038ff45826bdf02224bdf67
Travis Wheeler <travis.wheeler130@yahoo.com>
2019-10-03 18:04:58 -0600

    another commit

---
+++ StarWarsAPI/StarWarsAPI/Models/Film.swift
@@ -0,0 +1,13 @@
+//
+//  Film.swift
+//  StarWarsAPI
+//
+//  Created by Travis Wheeler on 10/3/19.
+//  Copyright © 2019 Travis Wheeler. All rights reserved.
+//
+
+import Foundation
+
+struct Film: Decodable {
+    let title: String
+}
